caae411e4e7da3a4d5a6a913f2da83d29b1786dd
Roger J. Bos, CFA <roger.bos@gmail.com>
2023-08-24 15:18:09 -0400

    step 3

--- src/index.js
+++ src/index.js
@@ -22,6 +22,15 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const web3 = __importStar(require("@solana/web3.js"));
 const sdk = __importStar(require("@hxronetwork/parimutuelsdk"));
@@ -29,3 +38,14 @@ const config = sdk.MAINNET_CONFIG;
 const rpc = web3.clusterApiUrl('mainnet-beta');
 const connection = new web3.Connection(rpc, 'confirmed');
 const parimutuelWeb3 = new sdk.ParimutuelWeb3(config, connection);
+// retrieve all of the Contests for the BTCUSD market pair.
+const market = sdk.MarketPairEnum.BTCUSD;
+const markets = sdk.getMarketPubkeys(config, market);
+// filter for the expiry interval, use the .filter() method on the markets array, and pass in a callback function that checks if the duration property of each element is equal to the desired expiry time in seconds.
+const marketTerm = 60; // The expires are in seconds, so this would be the 1 min
+const marketsByTime = markets.filter((market) => market.duration === marketTerm);
+console.log("marketsByTime");
+console.log(marketsByTime);
+const Paris = () => __awaiter(void 0, void 0, void 0, function* () {
+    const parimutuels = yield parimutuelWeb3.getParimutuels(marketsByTime, 5);
+});
--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,19 @@ const rpc = web3.clusterApiUrl('mainnet-beta')
 const connection = new web3.Connection(rpc, 'confirmed')
 
 const parimutuelWeb3 = new sdk.ParimutuelWeb3(config, connection)
+
+// retrieve all of the Contests for the BTCUSD market pair.
+const market = sdk.MarketPairEnum.BTCUSD
+const markets = sdk.getMarketPubkeys(config, market);
+
+// filter for the expiry interval, use the .filter() method on the markets array, and pass in a callback function that checks if the duration property of each element is equal to the desired expiry time in seconds.
+const marketTerm = 60 // The expires are in seconds, so this would be the 1 min
+
+const marketsByTime = markets.filter( (market) => market.duration === marketTerm );
+console.log("marketsByTime")
+console.log(marketsByTime)
+
+const Paris = async () => {
+    const parimutuels = await parimutuelWeb3.getParimutuels(marketsByTime, 5);
+}
+
